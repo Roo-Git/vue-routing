121ff34239b64ec8afe75140f5e014b844d588a3
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-06 09:07:56 +0200

    registered routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-
-const routes = [];
+import TeamsList from "../components/teams/TeamsList.vue";
+import UsersList from "../components/users/UsersList.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes: [
+    { path: "/teams", component: TeamsList },
+    { path: "/users", component: UsersList },
+  ],
 });
 
 export default router;
