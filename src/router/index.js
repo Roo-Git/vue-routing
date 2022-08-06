a829b870a3db6be2d020491d0ec4f3ef3f14bee4
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-06 10:06:47 +0200

    passing data with route params

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import TeamsList from "../components/teams/TeamsList.vue";
 import UsersList from "../components/users/UsersList.vue";
+import TeamMembers from "../components/teams/TeamMembers.vue";
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: "/teams", component: TeamsList },
+    { path: "/teams/:teamId", component: TeamMembers }, // dynamic segment
     { path: "/users", component: UsersList },
   ],
 });
