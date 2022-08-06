b03afecf47a3199a0115f23c423bd024cde65a29
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-06 11:16:35 +0200

    redirect path

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import TeamMembers from "../components/teams/TeamMembers.vue";
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
+    { path: "/", redirect: "/teams" }, // redirection to teams
     { path: "/teams", component: TeamsList },
     { path: "/teams/:teamId", component: TeamMembers }, // dynamic segment
     { path: "/users", component: UsersList },
