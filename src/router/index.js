c368c07acabdc911174f0d036199b9712e084757
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-08 10:08:20 +0200

    add nested route

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,14 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: "/", redirect: "/teams" }, // redirection to teams
-    { path: "/teams", component: TeamsList },
-    { path: "/teams/:teamId", component: TeamMembers, props: true }, // dynamic segment
+
+    {
+      path: "/teams",
+      component: TeamsList,
+      children: [
+        { path: "/teams/:teamId", component: TeamMembers, props: true }, // dynamic segment
+      ],
+    },
     { path: "/users", component: UsersList },
     // { path: "/users", component: UsersList, alias: '/'},  // redirection with alias
     { path: "/:notFound(.*)", redirect: "/teams" }, // catch all routes  // component: Not found
